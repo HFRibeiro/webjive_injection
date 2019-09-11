608030fcacea23fe47fb045f0b54f61689dd8d2b
HFRibeiro <ribeirh02@gmail.com>
2019-09-11 13:26:31 +0100

    new copy

--- src/dashboard/widgets/LedDisplay.tsx
+++ src/dashboard/widgets/LedDisplay.tsx
@@ -10,6 +10,7 @@ import {
 
 type Inputs = {
   showDevice: BooleanInputDefinition;
+  showValue: BooleanInputDefinition;
   compare: NumberInputDefinition;
   relation: SelectInputDefinition;
   attribute: AttributeInputDefinition;
@@ -27,32 +28,69 @@ function Led(props) {
   if (props.value === undefined) {
     classColor = classColor + " led-blank";
   }
-  return <span className={classColor}>{props.value}</span>;
+  
+  const pxSize = props.actualWidth/10+"px";
+  const marginLED = props.actualWidth/20+"px";
+  const marginLEDTOP = props.actualWidth/40+"px";
+  
+  const styleLED: CSSProperties = {height: pxSize, width: pxSize, borderRadius: pxSize, marginTop: marginLEDTOP};
+
+  return <span style={styleLED} className={classColor}>{props.value}</span>;
 }
+
 
 class LedReadOnly extends Component<Props> {
   public render() {
     const { name } = this.deviceAndAttribute();
 
-    const value = this.checkCondition();
-    const style: CSSProperties = { padding: "0.5em", whiteSpace: "nowrap" };
+    const {actualWidth} = this.props;
+    const fontSizeC = actualWidth/25;
+    const marginLabel = actualWidth/10;
 
-    const inner = this.props.inputs.showDevice ? (
-      <Fragment>
-        {name}:{" "}
+    const value = this.checkCondition();
+    const style: CSSProperties = {height: "30px",padding: "0.1em", whiteSpace: "nowrap", fontSize: fontSizeC};
+
+    const styleLable: CSSProperties = {marginLeft: marginLabel, fontSize: (fontSizeC*2)};
+
+    let inner;
+
+    if(this.props.inputs.showDevice && this.props.inputs.showValue)
+    {
+      inner = (
+        <Fragment>
         <Led
           condition={value}
           color={this.props.inputs.classColor}
           value={this.props.inputs.attribute.value}
+          actualWidth={this.props.actualWidth}
+        />
+        <a style={styleLable}>:{" "}{name}</a>
+      </Fragment>
+      );
+    }
+    else if(!this.props.inputs.showDevice && this.props.inputs.showValue)
+    {
+      inner = (
+        <Fragment>
+        <Led
+          condition={value}
+          color={this.props.inputs.classColor}
+          value={this.props.inputs.attribute.value}
+          actualWidth={this.props.actualWidth}
         />
       </Fragment>
-    ) : (
-      <Fragment>
+      );
+    }
+    else
+    {
+      inner = (
+        <Fragment>
         <Led condition={value} color={this.props.inputs.classColor} />
       </Fragment>
-    );
+      );
+    }
 
-    return <div style={style}>{inner}</div>;
+    return <div style={style}>{inner} </div>;
   }
 
   private checkCondition(): any {
@@ -101,7 +139,7 @@ class LedReadOnly extends Component<Props> {
 export const definition: WidgetDefinition<Inputs> = {
   type: "LED_DISPLAY",
   name: "Led Display",
-  defaultWidth: 10,
+  defaultWidth: 6,
   defaultHeight: 2,
   inputs: {
     attribute: {
@@ -161,6 +199,11 @@ export const definition: WidgetDefinition<Inputs> = {
     showDevice: {
       type: "boolean",
       label: "Attribute Value",
+      default: false
+    },
+    showValue: {
+      type: "boolean",
+      label: "Display Value",
       default: false
     }
   }
